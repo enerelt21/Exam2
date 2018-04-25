b1ef226661076c734de3d10eaf65c5a7c6fec2fa
enerelt21 <32151102+enerelt21@users.noreply.github.com>
2018-04-25 13:45:57 -0700

    Add files via upload

---
+++ Exam_02/exam2/BirthdayParty.h
@@ -0,0 +1,72 @@
+#ifndef _BIRTHDAY_PARTY_H_
+#define _BIRTHDAY_PARTY_H_
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <string>
+#include "Party.h"
+#include "BirthdayPartyTicket.h"
+
+// FIXME 2: Create BirthdayParty and BirthdayPartyTicket to host birthday parties!
+// FIXME 3: Update PartyFactory::factory() to allow it to be the ONLY method for creating a BirthdayParty
+/**
+* BirthdayParty is derived from Party
+*/
+class BirthdayParty : public Party
+{
+private:
+	std::list<std::string> room; // Data structure for containing the party goers in this dinner party
+
+								 /**
+								 * Constructor for creating a DinnerParty
+								 */
+	BirthdayParty() {}
+
+	friend class PartyFactory;
+
+public:
+
+	/**
+	* Add a person to the party
+	* @param name the name of the party goer
+	*/
+	PartyTicket * add(std::string name)
+	{
+		/**
+		* FIXME 1a:
+		*   Pseudo-code
+		*    1. add name to the front of room
+		*    2. get iterator to the front of the room (this points to the recently added party goer)
+		*    3. create a new dinner party ticket (DinnerPartyTicket) with its constructor
+		*            Pass to the constructor "this" party, and the iterator from step 2
+		*    4. return this new party ticket
+		*/
+		room.push_front(name);
+		std::list<std::string>::iterator it;
+		it = room.begin();
+		//BirthdayParty *the_party = new BirthdayParty;
+		//BirthdayPartyTicket(the_party, it);
+		return new BirthdayPartyTicket(this, it);
+	}
+
+	/**
+	* List all the persons currently in the party
+	*/
+	void list() {
+		// FIXME 1b: iterate through all the persons here.
+		std::list<std::string>::iterator it;
+		for (it = room.begin(); it != room.end(); ++it)
+			std::cout << (*it) << std::endl;
+	}
+
+	/**
+	* Remove the person identified by the iterator from the party
+	*/
+	void remove(std::list<std::string>::iterator it) {
+		//FIXME 1c: someone's getting out of hand. Let's ask them to leave.
+		room.remove(*it);
+	}
+};
+
+#endif
---
+++ Exam_02/exam2/BirthdayPartyTicket.cpp
@@ -0,0 +1,10 @@
+#include "BirthdayPartyTicket.h"
+#include "BirthdayParty.h"
+
+/**
+* Implements the leave method
+*/
+void BirthdayPartyTicket::leave() {
+	BirthdayParty * actual_party = dynamic_cast<BirthdayParty *>(the_party); // down-cast base class to derived class
+	actual_party->remove(me); // call remove DinnerParty remove method to erase party goes identified by "me"
+}
